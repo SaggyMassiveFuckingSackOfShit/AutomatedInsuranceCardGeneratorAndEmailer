281a2bf8bdd7a33e8b3b763e69a1457a27148f90
itszaih <61492950+eddrindesiderio@users.noreply.github.com>
2025-03-03 12:40:25 +0800

    progress.php

---
+++ progress.php
@@ -0,0 +1,19 @@
+<?php
+header('Content-Type: application/json');
+
+$progressFile = "outputs/progress.txt";
+
+if (!file_exists($progressFile)) {
+    file_put_contents($progressFile, "0");
+}
+
+$progress = file_get_contents($progressFile);
+
+// If progress is stuck at 100% before a new request, reset it
+if ((int)$progress >= 100) {
+    $progress = 0;
+    file_put_contents($progressFile, "0");
+}
+
+echo json_encode(["progress" => $progress]);
+?>
